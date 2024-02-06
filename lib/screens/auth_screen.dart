3db13de7d2ca39c762c83ca73d0e5782f05ab943
auza0002 <auzajuan1@gmail.com>
2024-02-06 15:50:13 -0500

    multiDexEnable set to true

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:chat_app/widgets/user_image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -59,7 +60,16 @@ class _AuthScreenState extends State<AuthScreen> {
             .child("${userCredentials.user!.uid}.jpg");
         await firebaseStorageRef.putFile(_selectedImage!);
         final imgURl = await firebaseStorageRef.getDownloadURL();
-        print(imgURl);
+        await FirebaseFirestore.instance
+            .collection("users")
+            .doc("${userCredentials.user!.uid}")
+            .set(
+          {
+            "username": "to be done...",
+            "email": _enteredEmail,
+            "image_url": imgURl,
+          },
+        );
       }
     } on FirebaseAuthException catch (userCredentialsError) {
       setState(() {
