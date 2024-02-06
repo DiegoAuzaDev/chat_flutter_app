6ad7c6969ededf99bf3b2e9850501f5cb259cece
auza0002 <auzajuan1@gmail.com>
2024-02-06 14:46:09 -0500

    Adding Firebase Storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/screens/auth_screen.dart';
 import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/splash.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Brightness themeBrightness = Brightness.light;
+  Brightness themeBrightness = Brightness.dark;
 
   @override
   Widget build(BuildContext context) {
@@ -33,18 +34,19 @@ class _MyAppState extends State<MyApp> {
           seedColor: const Color.fromARGB(255, 38, 101, 137),
           secondary: const Color.fromARGB(255, 80, 96, 110),
           tertiary: const Color.fromARGB(255, 100, 89, 123),
-          surface: const Color.fromARGB(255, 247, 249, 255),
         ),
         textTheme: GoogleFonts.soraTextTheme(),
       ),
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
           if (snapshot.hasData) {
             return const ChatScreen();
-          } else {
-            return const AuthScreen();
           }
+          return const AuthScreen();
         },
       ),
     );
